4df0a3eca721ff8d401606202c6add628ecfe52d
pcasgo <pcasgo@gmail.com>
2019-10-08 17:45:07 -0300

    [FIX]: MOdificacioes estilos

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import {
+    SUN,
+} from './../../constants/weathers'
+
+const data = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+}
 
 const WeatherLocation = () => (
     <div className="weatherLocationCont">
-        <Location city={'Barcelona'}></Location>
-        <WeatherData></WeatherData>
+        <Location city={ 'Barcelona'}></Location>
+        <WeatherData data={data}></WeatherData>
     </div>
 )
 
