6907b4ea3f025179c998a8e8330bf57a18ce9545
pcasgo <pcasgo@gmail.com>
2019-10-11 16:28:37 -0300

    [FIX]: Ejemplo componenet function a class component

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    WINDY,
-} from './../../../constants/weathers';
 import './styles.css';
+import PropTypes from 'prop-types';
 
-const WeatherData = () => (
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => (
     <div className='weatherDataCont'>
-        <WeatherTemperature 
-            temperature={20} 
-            weatherState={SUN} 
+        <WeatherTemperature
+            temperature={temperature}
+            weatherState={weatherState}
         />
-        <WeatherExtraInfo humidity={80} wind=  {'10 m/s'} />
+        <WeatherExtraInfo humidity={humidity} wind={wind} />
     </div>
 );
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
@@ -7,17 +7,21 @@ import {
 } from './../../constants/weathers'
 
 const data = {
-    temperature: 5,
+    temperature: 15,
     weatherState: SUN,
     humidity: 10,
     wind: '10 m/s',
 }
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={ 'Barcelona'}></Location>
-        <WeatherData data={data}></WeatherData>
-    </div>
-)
+class WeatherLocation extends Component {
+    render() {
+        return (
+            <div className="weatherLocationCont">
+                <Location city={'Barcelona'}></Location>
+                <WeatherData data={data}></WeatherData>
+            </div>
+        );
+    }
+}
 
 export default WeatherLocation;
